c0f60c6cddeb445b2378d6274e99b8b66e5167c5
Bulat <bulat0talipov@gmail.com>
2022-02-27 11:55:56 +0300

    added completed page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
-$ (function (){
-  
+let headerTopBtn = document.querySelector('.header-top__btn');
+let headerBotton = document.querySelector('.header-bottom');
+
+headerTopBtn.addEventListener('click', function(){
+  headerTopBtn.classList.toggle('header-top__btn--active');
+  headerBotton.classList.toggle('header-bottom--active');
 });
